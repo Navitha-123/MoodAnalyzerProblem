c4284582af14747dbbaf40b42f05f8aaf0c171ef
navitha <navithareddy112@gmail.com>
2022-09-21 21:18:52 +0530

    [navitha] Refactor

---
+++ MoodAnalyzerProblem/Refactor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class Refactor
+    {
+        public string message;
+
+        public Refactor(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            message = message.ToLower();
+            if(message.Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
