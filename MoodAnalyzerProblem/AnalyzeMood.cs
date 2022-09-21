cf5a5b739eca9b96c77fc41e4da84ecd38b537a9
navitha <navithareddy112@gmail.com>
2022-09-21 21:07:05 +0530

    [navitha] AnalyzeMood

---
+++ MoodAnalyzerProblem/AnalyzeMood.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class AnalyzeMood
+    {
+        public string message;
+
+        public AnalyzeMood(string message)
+        {
+            this.message = message;
+        }
+        public string Mood()
+        {
+            message = message.ToLower();
+            if (message.Contains("happy"))
+                return "happy";
+            else
+                return "sad";
+
+        }
+    }
+}
+
+    
+
